641949ab88e93c054bd562acb85a81ea29690ac3
ligen <ligen@keenvision.cn>
2016-08-01 19:18:42 +0800

    ci with mocha

---
+++ test.js
@@ -0,0 +1,18 @@
+'use strict'
+
+require('should');
+
+describe('calc', () => {
+  it('1+1=2', ()=>{
+
+    (1+1).should.eql(2);
+    
+  })
+
+  it('1+1=3', ()=>{
+
+    (1+1).should.eql(3);
+    
+  })
+
+})
